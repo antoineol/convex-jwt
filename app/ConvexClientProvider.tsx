c7b5beb2dc3c0c0cf71303ebd37d0aa773cb8b9f
Antoine Ory-Lamballe <antoine.orylamballe@faktus.eu>
2025-11-23 16:07:16 +0100

    add verbose logs

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -4,7 +4,24 @@ import { useJwtAuth } from "@/hooks/useJwtAuth";
 import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+  expectAuth: true,
+  verbose: true,
+  logger: {
+    logVerbose: (...args) => {
+      console.log('[verbose]', ...args);
+    },
+    log: (...args) => {
+      console.log('[log]', ...args);
+    },
+    warn: (...args) => {
+      console.warn('[warn]', ...args);
+    },
+    error: (...args) => {
+      console.error('[error]', ...args);
+    },
+  }
+});
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
